4dddc8c42aaf3601729f5f9b4b9b80dfd1aba746
Ent38 <63259634+Ent38@users.noreply.github.com>
2022-03-31 11:31:36 -0700

    modiff

--- app/User.php
+++ app/User.php
@@ -2,8 +2,19 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use DateTimeInterface;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Notifications\ResetPassword;
+use App\Client;
+use App\Role;
+use App\RoleUser;
+use App\UserClient;
+
+
 
 /**
  * @property int $id
@@ -15,13 +26,45 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $created_at
  * @property string $updated_at
  */
-class User extends Model
+
+class User extends Authenticatable
 {
-    use HasFactory;
+    use Notifiable;
+
+    public $table = 'users';
+
     /**
+     * The attributes that are mass assignable.
+     *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'email_verified_at', 'password', 'remember_token', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'name', 'email', 'email_verified_at', 'password', 'remember_token', 'created_at', 'updated_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['updated_at','created_at','deleted_at','email_verified_at'];
+
+    public function setPasswordAttribute($input)
+    {
+        if ($input) {
+            $this->attributes['password'] = app('hash')->needsRehash($input) ? Hash::make($input) : $input;
+        }
+
+    }
 
     public function clients()
     {
@@ -36,3 +79,4 @@ class User extends Model
             ->withTimestamps();
     }
 }
+
