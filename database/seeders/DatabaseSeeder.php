daabe9a8b7edc5a0ce89339cf1a2055ee1ad4e7a
mMaJames9 <anthonymisse85@gmail.com>
2022-04-08 10:22:25 +0100

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PermissionSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(CategorySeeder::class);
-        $this->call(ClientSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(PermissionRoleSeeder::class);
         $this->call(RoleUserSeeder::class);
-        $this->call(ClientUserSeeder::class);
-        $this->call(FactureServiceSeeder::class);
+        // $this->call(CategorySeeder::class);
+        // $this->call(ClientSeeder::class);
+        // $this->call(ClientUserSeeder::class);
+        // $this->call(FactureServiceSeeder::class);
     }
 }
